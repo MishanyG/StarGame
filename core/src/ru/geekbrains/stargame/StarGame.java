87cf0fe249b81e80c28fd03df95394c71baa1cb8
Misha Glushak <flatron_777@mail.ru>
2020-04-03 14:20:08 +1000

    Не успел вовремя сделать, пришлось проект переделывать, был какой-то глюк с текстурами, не отрисовывались некоторые текстуры, долго мучался, в итоге всё переделал и заработало, но времени на остальное не хватило.

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -1,32 +1,13 @@
 package ru.geekbrains.stargame;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import ru.geekbrains.stargame.base.Logo;
 import ru.geekbrains.stargame.screen.MenuScreen;
 
-public class StarGame extends Game
-{
-	MenuScreen menuScreen;
-	SpriteBatch batch;
-	Logo logo;
+public class StarGame extends Game {
 
 	@Override
-	public void create()
-	{
-		menuScreen = new MenuScreen();
-		batch = new SpriteBatch();
-		logo = new Logo();
-		logo.show();
-	}
-	@Override
-	public void render()
-	{
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		menuScreen.render(batch);
-		logo.render(batch);
+	public void create() {
+		setScreen(new MenuScreen(this));
 	}
 }
---
+++ core/src/ru/geekbrains/stargame/sprites/Ship.java
@@ -0,0 +1,160 @@
+package ru.geekbrains.stargame.sprites;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.stargame.base.Sprite;
+import ru.geekbrains.stargame.exception.GameException;
+import ru.geekbrains.stargame.math.Rect;
+
+public class Ship extends Sprite {
+
+    private static final int INV_POINT = -1;
+
+    private Rect worldBounds;
+    private final Vector2 v0;
+    private final Vector2 v;
+    private boolean pressLeft;
+    private boolean pressRight;
+    private int leftPoint = INV_POINT;
+    private int rightPoint = INV_POINT;
+
+    public Ship(TextureAtlas atlas) throws GameException {
+        super(atlas.findRegion("main_ship"), 1, 2, 2);
+        v0 = new Vector2(0.5f, 0);
+        v = new Vector2();
+    }
+    @Override
+    public void resize(Rect worldBounds)
+    {
+        this.worldBounds = worldBounds;
+        setHeightProportion(0.15f);
+        setBottom(worldBounds.getBottom() + 0.01f);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button)
+    {
+        if (touch.x < worldBounds.pos.x)
+        {
+            if (leftPoint != INV_POINT)
+            {
+                return false;
+            }
+            leftPoint = pointer;
+            moveLeft();
+        }
+        else
+        {
+            if (rightPoint != INV_POINT)
+            {
+                return false;
+            }
+            rightPoint = pointer;
+            moveRight();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button)
+    {
+        if (pointer == leftPoint)
+        {
+            leftPoint = INV_POINT;
+            if (rightPoint != INV_POINT)
+            {
+                moveRight();
+            }
+            else
+            {
+                stop();
+            }
+        }
+        else
+        {
+            if (pointer == rightPoint)
+            {
+                rightPoint = INV_POINT;
+                if (leftPoint != INV_POINT)
+                {
+                    moveLeft();
+                }
+                else
+                {
+                    stop();
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean keyDown(int keycode)
+    {
+        switch (keycode)
+        {
+            case Input.Keys.A:
+            case Input.Keys.LEFT:
+                pressLeft = true;
+                moveLeft();
+                break;
+            case Input.Keys.D:
+            case Input.Keys.RIGHT:
+                pressRight = true;
+                moveRight();
+                break;
+        }
+        return false;
+    }
+
+    public boolean keyUp(int keycode)
+    {
+        switch (keycode)
+        {
+            case Input.Keys.A:
+            case Input.Keys.LEFT:
+                pressLeft = false;
+                if (pressLeft)
+                {
+                    moveLeft();
+                }
+                else
+                {
+                    stop();
+                }
+            case Input.Keys.D:
+            case Input.Keys.RIGHT:
+                pressRight = false;
+                if (pressRight)
+                {
+                    moveRight();
+                }
+                else
+                {
+                    stop();
+                }
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public void update(float delta)
+    {
+        pos.mulAdd(v, delta);
+    }
+
+    private void moveRight ()
+    {
+        v.set(v0);
+    }
+    private void moveLeft ()
+    {
+        v.set(v0).rotate(180);
+    }
+    private void stop ()
+    {
+        v.setZero();
+    }
+}
